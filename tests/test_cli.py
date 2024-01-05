bd9bbd2be2351e4e518a58a00b4de8688ff218d9
Frank Hoffmann <15r10nk-git@polarbit.de>
2024-01-05 08:06:30 +0100

    test: test for correct help output

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,19 @@
 import subprocess as sp
 
+from inline_snapshot import snapshot
+
 
 def test_cli():
-    result = sp.run(["canonical-imports"])
+    result = sp.run(["canonical-imports", "--help"], capture_output=True)
     assert result.returncode == 0
+    assert result.stdout.decode() == snapshot(
+        """\
+Usage: canonical-imports [OPTIONS] [FILES]...
+
+Options:
+  --no [public-private|into-init]
+                                  Exclude specific imports
+  -w, --write TEXT                write changed imports
+  --help                          Show this message and exit.
+"""
+    )
